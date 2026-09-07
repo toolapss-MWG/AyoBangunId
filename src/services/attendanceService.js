881bae1f54e75f363e068894e63888970d1b6e7b
toolapss-MWG <whimasnanjar4@gmail.com>
2026-09-07 16:12:13 +0700

    Update attendanceService.js

--- src/services/attendanceService.js
+++ src/services/attendanceService.js
@@ -1,3 +1,10 @@
-js
+import { db, ref, get, set, update } from "../firebase.js";
 
-import { db, ref, get, set, update } from "../firebase.js";  export async function getAttendance(pid, dateISO){   const snap = await get(ref(db, projects/${pid}/attendance/${dateISO}));   return snap.exists() ? snap.val() : {}; }  export async function setAttendanceItem(pid, dateISO, uid, payload){   await update(ref(db, projects/${pid}/attendance/${dateISO}/${uid}), payload); }
+export async function getAttendance(pid, dateISO){
+  const snap = await get(ref(db, `projects/${pid}/attendance/${dateISO}`));
+  return snap.exists() ? snap.val() : {};
+}
+
+export async function setAttendanceItem(pid, dateISO, uid, payload){
+  await update(ref(db, `projects/${pid}/attendance/${dateISO}/${uid}`), payload);
+}
